3e6dd23b8fe03cdea2c0ccd67af8c21341b193b7
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-01-20 08:14:30 +0100

    Remove the global variables

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,9 @@
 # !/usr/bin/env ruby
 
-$options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-$options_copy = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+OPTIONS= [1, 2, 3, 4, 5, 6, 7, 8, 9]
+OPTIONS_COPY = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-def display_game_status (options_p)
+def display_game_status(options_p)
   puts '      *       *      '
   puts "   #{options_p[0]}  *   #{options_p[1]}   *   #{options_p[2]}  "
   puts '      *       *       '
@@ -17,16 +17,16 @@ def display_game_status (options_p)
   puts '      *       *       '
 end
 
-def play_the_game(player_name, i)
+def play_the_game(player_name, int)
   puts "#{player_name}, Please pick an option"
-  display_game_status($options)
-  player_option = gets.chomp()
+  display_game_status(OPTIONS)
+  player_option = gets.chomp
   player_option = player_option.to_i
-  if $options_copy.include?(player_option)
-    $options_copy = $options_copy.reject { |choice| choice == player_option }
-    $options[player_option-1] = i.odd? ? "o" : "x"
+  if OPTIONS_COPY.include?(player_option)
+    OPTIONS_COPY = OPTIONS_COPY.reject { |choice| choice == player_option }
+    OPTIONS[player_option - 1] = int.odd? ? 'o' : 'x'
   else
-    play_the_game(play_the_game, i)
+    play_the_game(play_the_game, int)
   end
 end
 
@@ -65,13 +65,13 @@ end
 #   end
 # end
 
-def ask_the_name (player)
+def ask_the_name(player)
   puts "#{player}, please put your name"
-  player_name = gets.chomp()
+  player_name = gets.chomp
   if player_name.length >= 2
     puts "Welcome #{player_name}"
   else
-    puts "Please enter a correct name"
+    puts 'Please enter a correct name'
     puts 'Name should be atleast two characters'
     ask_the_name(player)
   end
@@ -88,22 +88,20 @@ when 'Y'
   # Play the game
   players = []
   2.times do |i|
-    players << ask_the_name("Player number #{i+1}")
+    players << ask_the_name("Player number #{i + 1}")
   end
-  puts "#{players}"
+  puts players.to_s
 
   9.times do |i|
     if i.odd?
-      #second player has to play
+      # second player has to play
       play_the_game(players[1], i)
     elsif i.even?
       # it's first player's turn
       play_the_game(players[0], i)
-    else
-      #end the game
     end
-  end 
-  display_game_status($options)
+  end
+  display_game_status(OPTIONS)
   # Playing the Game
   # tic_tac_toe_game(player_one, player_two)
 when 'N'
