26a19e68dcdaab6b6f31d6fc4057451eda8e0f94
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-01-20 17:41:36 +0100

    Update the game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,25 @@
 # !/usr/bin/env ruby
+class Player
+  attr_accessor :name, :options, :options_to_display, :options_checker
 
-options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-options_copy = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-updated_options = []
+  def initialize(name, options)
+    @name = name
+    @options = options
+    @options_to_display = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @options_checker = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def update_options(player_option, symbol)
+    if @options_checker.include?(player_option)
+      # update the options_to_display array
+      @options_checker.reject { |choice| choice == player_option }
+      @options_to_display[player_option - 1] = symbol
+    else
+      puts 'Please select a correct option'
+      update_options(player_option, symbol)
+    end
+  end
+end
 
 def display_game_status(options_p)
   puts '      *       *      '
@@ -18,19 +35,11 @@ def display_game_status(options_p)
   puts '      *       *       '
 end
 
-def play_the_game(player_name, int, options, options_copy)
+def play_the_game(player_name, options)
   puts "#{player_name}, Please pick an option"
   display_game_status(options)
   player_option = gets.chomp
-  player_option = player_option.to_i
-  if options_copy.include?(player_option)
-    options_copy = options_copy.reject { |choice| choice == player_option }
-    options[player_option - 1] = int.odd? ? 'o' : 'x'
-  else
-    play_the_game(play_the_game, int)
-  end
-  updated_options = [options, options_copy]
-  updated_options
+  player_option.to_i
 end
 
 def ask_the_name(player)
@@ -55,27 +64,34 @@ case user_response.upcase
 when 'Y'
   # Play the game
   players = []
-  2.times do |i|
-    players << ask_the_name("Player number #{i + 1}")
-  end
-  puts players.to_s
 
-  9.times do |i|
-    if i.odd?
-      # second player has to play
-    updated_options = play_the_game(players[1], i, options, options_copy)
-    options = updated_options[0]
-    options_copy = updated_options[1]
-    elsif i.even?
-      # it's first player's turn
-      play_the_game(players[0], i, options, options_copy)
-      options = updated_options[0]
-      options_copy = updated_options[1]
-    end
+  (1..2).each do |i|
+    players << ask_the_name("Player number #{i}")
   end
-  display_game_status(options)
-  # Playing the Game
-  # tic_tac_toe_game(player_one, player_two)
+  # puts players.to_s
+  player_one = Player.new(players[0], [])
+  player_two = Player.new(players[1], [])
+  finished = false
+  j = 0
+  puts j
+  until finished
+    if j.odd?
+      # second player has to play
+      player_option = play_the_game(player_two.name, player_one.options_to_display)
+      player_two.update_options(player_option, 'o')
+      player_one.options_checker = player_two.options_checker
+      player_one.options_to_display = player_two.options_to_display
+    elsif j.even?
+      # it's first player's turn
+      player_option = play_the_game(player_one.name, player_one.options_to_display)
+      player_one.update_options(player_option, 'x')
+      player_two.options_checker = player_one.options_checker
+      player_two.options_to_display = player_one.options_to_display
+    end
+    j += 1
+    finished = true if j >= 9
+  end
+  display_game_status(player_two.options_to_display)
 when 'N'
   # Close the game
   puts 'Great! You could check later when youre ready'
